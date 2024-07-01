5f2e63d585457952d0355386859a84e91db83162
Ha Jinyoung <230135@ict.ac.kr>
2024-07-01 17:07:48 +0900

    initial commit

--- sbb/src/main/java/com/mysite/sbb/HelloController.java
+++ sbb/src/main/java/com/mysite/sbb/HelloController.java
@@ -9,6 +9,13 @@ public class HelloController {
 	@GetMapping("/hello")
 	@ResponseBody
 	public String hello() {
-		return "Hello Spring Boot Board";
+		return "Hello Spring Boot Board!!!!!!!!";
+	}
+
+	@GetMapping("/hellohtml")
+	@ResponseBody
+	public String hellohtml(){
+		return "<html><body><h1 style=\"color:blue;\">안녕하세요</h1><body></html>";
 	}
 }
+
